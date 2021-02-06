c67fb8abd31b8761966a8a06a015d264d58e048d
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2021-02-06 14:59:54 +0900

    引数を受け取った場合の実装

--- 06.wc/wc_command.rb
+++ 06.wc/wc_command.rb
@@ -2,7 +2,56 @@
 # frozen_string_literal: true
 
 require './option'
+require 'pathname'
 
 option = Option.new
 
-p option.extras
+def output_for_arguments(option)
+  arguments = option.extras.map { |argument| Pathname(argument) }
+  output_list = Hash.new { |hash, key| hash[key] = {} }
+
+  arguments.each do |argument|
+    if argument.directory?
+      puts "wc: #{argument}: read: Is a directory"
+    elsif argument.exist?
+      output_list = create_list(output_list, argument)
+      output(output_list, argument, option)
+    else
+      puts "wc: #{argument}: open: No such file or directory"
+    end
+  end
+  output_total(output_list, option) if arguments.size >= 2
+end
+
+def create_list(output_list, file)
+  output_list[:"#{file}"][:lines] = IO.readlines(file).size
+  output_list[:"#{file}"][:words] = IO.read(file).split(' ').size
+  output_list[:"#{file}"][:size] = File.stat(file).size
+  output_list
+end
+
+def output(output_list, file, option)
+  print output_list[:"#{file}"][:lines].to_s.rjust(8)
+  unless option.lists?
+    print output_list[:"#{file}"][:words].to_s.rjust(8)
+    print output_list[:"#{file}"][:size].to_s.rjust(8)
+  end
+  print " #{file}\n"
+end
+
+def output_total(output_list, option)
+  total = Hash.new(0)
+  output_list.each_value do |value|
+    total[:lines] += value[:lines]
+    unless option.lists?
+      total[:words] += value[:words]
+      total[:size] += value[:size]
+    end
+  end
+  total.each_value do |value|
+    print value.to_s.rjust(8)
+  end
+  print " total\n"
+end
+
+output_for_arguments(option)
