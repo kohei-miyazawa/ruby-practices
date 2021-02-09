63fb1e0d3cd467844681a8cf655b962586dbff39
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2021-02-09 15:44:33 +0900

    ファイルが見つからない場合に対応

--- 09.wc_object/filelist.rb
+++ 09.wc_object/filelist.rb
@@ -1,26 +1,40 @@
 # frozen_string_literal: true
 
 require_relative './file'
+require 'pathname'
 
 class Wc
   class FileList
-    attr_reader :files
+    attr_reader :arguments, :files
 
     def initialize(option)
-      files = option.extras
-      @files = files.map { |file| Wc::File.new(file) }
+      @arguments = option.extras.map { |argument| Pathname(argument) }
+      @files = @arguments.map { |argument| Wc::File.new(argument) if argument.file? }.compact
     end
 
     def display(option)
-      files.each do |file|
-        print file.lines.to_s.rjust(8)
-        unless option.lists?
-          print file.words.to_s.rjust(8)
-          print file.size.to_s.rjust(8)
+      arguments.each do |argument|
+        if argument.directory?
+          puts "wc: #{argument}: read: Is a directory"
+        elsif !argument.exist?
+          puts "wc: #{argument}: open: No such file or directory"
+        else
+          display_file(argument, option)
         end
-        puts " #{file.name}"
       end
-      display_total(option) if option.extras.size >= 2
+      display_total(option) if arguments.size >= 2
+    end
+
+    private
+
+    def display_file(argument, option)
+      file = files.find { |f| f.name == argument }
+      print file.lines.to_s.rjust(8)
+      unless option.lists?
+        print file.words.to_s.rjust(8)
+        print file.size.to_s.rjust(8)
+      end
+      puts " #{file.name}"
     end
 
     def display_total(option)
